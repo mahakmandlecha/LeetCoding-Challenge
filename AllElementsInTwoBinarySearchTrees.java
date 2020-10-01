f3a12e87a3f9d59be7fc2e913a6670b76800b1b2
Vishant93 <vishantk@gmail.com>
2020-10-01 11:41:23 -0700

    All elements in two binary search trees solution added.

--- AllElementsInTwoBinarySearchTrees.java
+++ AllElementsInTwoBinarySearchTrees.java
@@ -1,19 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-class TreeNode {
-      int val;
-      TreeNode left;
-      TreeNode right;
-      TreeNode() {}
-      TreeNode(int val) { this.val = val; }
-      TreeNode(int val, TreeNode left, TreeNode right) {
-          this.val = val;
-          this.left = left;
-          this.right = right;
-      }
-  }
 class AllElementsInTwoBinarySearchTrees {
     public List<Integer> inOrder(TreeNode root, List<Integer> list){
         if(root == null) return list;
