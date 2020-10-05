53696879bb1d6a066f461a022a4032e88acc2ae1
Devanshi Jain <devanshialokjain@gmail.com>
2020-10-05 19:21:17 +0530

    Two city scheduling in java:corrected

--- TwoCityScheduling.java
+++ TwoCityScheduling.java
@@ -1,19 +1,18 @@
-import java.util.*;
-
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter value of n");
-		int n = sc.nextInt();
-		int[][] arr = new int[n][2];
-		System.out.println("Enter aCosti and bCosti");
-		int answer = 0;
-		for(int i = 0;i<n;i++){
-		    arr[i][0] = sc.nextInt();
-		    arr[i][1] = sc.nextInt();
-		    answer+= Math.min(arr[i][0],arr[i][1]);
-		}
-		System.out.println("The total minimum cost is "+ answer);
-	}
+class Solution {
+    public int twoCitySchedCost(int[][] costs) {
+        int cost =0;
+        int l = costs.length;
+        for(int i=0;i<l;i++){
+            cost += costs[i][0];
+        }
+        int[] difference = new int[l];
+        for(int i=0;i<l;i++){
+            difference[i] = costs[i][1]-costs[i][0];
+        }
+        Arrays.sort(difference);
+        for(int i=0;i<l/2;i++){
+            cost+=difference[i];
+        }
+        return cost;
+    }
 }
