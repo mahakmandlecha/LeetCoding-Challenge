26608899bc48c3e28c128eed0c59b44aa1ed9d8f
Aman Thukral <thukral618@gmail.com>
2020-10-05 15:37:41 +0530

    Update TwoCityScheduling.java

--- TwoCityScheduling.java
+++ TwoCityScheduling.java
@@ -1,13 +1,17 @@
 class TwoCityScheduling {
     public int twoCitySchedCost(int[][] costs) {
-        int n=costs.length;
-        int m=costs[0].length;
-        int sum=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                sum+=Math.min(costs[0][j],costs[i][0]);
-            }
+        int n=costs.length/2;
+       int[] ref=new int[n*2];
+       int index=0,minCost=0;
+        for(int[] c:costs){
+            ref[index++]=c[1]-c[0];
+            minCost+=c[0];
+            
         }
-        return sum;
+        Arrays.sort(ref);
+        for(int i=0;i<n;i++){
+            minCost+=ref[i];
+        }
+        return minCost;
     }
 }
